1bc1bf48266234dedfe744321a2ed0aa73047e1b
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-05-22 16:17:38 +0700

    Create AdminViewProducts Screen

--- src/Screens/AdminViewProducts/index.js
+++ src/Screens/AdminViewProducts/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
+import Card from '../../components/Card';
+import AppLoading from '../../common/AppLoading';
+
+
+import {getProducts} from '../../requests/products';
+
 
 export default function AdminViewProducts() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    getProducts()
+      .then(res => setItems(res.products))
+      .catch(err => console.log(err));
+  }, []);
+
+  if (items?.length < 1) return <AppLoading />;
   return (
     <View>
-      <Text></Text>
+       <FlatList
+        data={items}
+        keyExtractor={items => items._id}
+        renderItem={({item}) => (
+          <Card
+            item={item}
+            // onPress={() => navigation.navigate('Drink Details', item)}
+          />
+        )}
+      />
     </View>
   );
 }
